e2df6beefd592dd9def800bde947a1067ff04f27
Vidya Trimbake <vidyatrimbake3@gmail.com>
2026-02-25 22:45:54 +0530

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/fintech/portfolio_backend_basic/PortfolioBackendBasicApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.portfolio_backend_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintech/portfolio_backend_basic/PortfolioBackendBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.portfolio_backend_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
